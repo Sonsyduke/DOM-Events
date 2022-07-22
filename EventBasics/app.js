4319caa96d85e5b2a560ce98c5aee3e2665ea825
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-22 09:34:26 +0200

    Onclick

--- EventBasics/app.js
+++ EventBasics/app.js
@@ -1,39 +1,17 @@
-const btn = document.querySelector('#v2');
+const v2 = document.querySelector("#v2");
 
-btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
-}
+v2.onclick = function () {
+  console.log("YOU CLICKED ME");
+  console.log("I HOPE IT WORKED!");
+};
 
 function scream() {
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
+  console.log("AAAAAAAAHHHHHHH");
+  console.log("STOP");
 }
 
-btn.onmouseenter = scream;
+v2.onmouseenter = scream;
 
-
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
-}
-
-
-const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert("CLICKED!");
-})
-
-function twist() {
-    console.log("TWIST!")
-}
-function shout() {
-    console.log("SHOUT!")
-}
-
-const tasButton = document.querySelector('#tas');
-
-// tasButton.onclick = twist;
-// tasButton.onclick = shout;
-
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+document.querySelector("h1").onclick = function () {
+  alert("you clicked the h1");
+};
