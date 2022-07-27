61b8fdd105180edfa2afb28030dc5635d2792758
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-27 13:12:00 +0200

    Form events exercise

--- FormEvents/app.js
+++ FormEvents/app.js
@@ -10,22 +10,35 @@
 //   input.value = "";
 // });
 
-const tweetForm = document.querySelector("#tweetForm");
-const tweetContainer = document.querySelector("#tweets");
-tweetForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const usernameInput = tweetForm.elements.username;
-  const tweetInput = tweetForm.elements.tweet;
-  addTweet(usernameInput.value, tweetInput.value);
-  usernameInput.value = "";
-  tweetInput.value = "";
-});
+// const tweetForm = document.querySelector("#tweetForm");
+// const tweetContainer = document.querySelector("#tweets");
+// tweetForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const usernameInput = tweetForm.elements.username;
+//   const tweetInput = tweetForm.elements.tweet;
+//   addTweet(usernameInput.value, tweetInput.value);
+//   usernameInput.value = "";
+//   tweetInput.value = "";
+// });
 
-function addTweet(username, tweet) {
-  const newTweet = document.createElement("li");
-  const bTag = document.createElement("b");
-  bTag.append(username);
-  newTweet.append(bTag);
-  newTweet.append(` - ${tweet}`);
-  tweetContainer.append(newTweet);
-}
+// function addTweet(username, tweet) {
+//   const newTweet = document.createElement("li");
+//   const bTag = document.createElement("b");
+//   bTag.append(username);
+//   newTweet.append(bTag);
+//   newTweet.append(` - ${tweet}`);
+//   tweetContainer.append(newTweet);
+// }
+
+const form = document.querySelector("form");
+const list = document.querySelector("#list");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const productInput = form.elements.product;
+  const quantityInput = form.elements.qty;
+  const item = document.createElement("li");
+  item.textContent = `${quantityInput.value} ${productInput.value}`;
+  list.appendChild(item);
+  productInput.value = "";
+  quantityInput.value = "";
+});
