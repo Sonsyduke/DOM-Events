410ae0af37356a8fa578e42f801e9019469042a7
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-27 12:33:19 +0200

    PreventDefault and submitting a form while keeping us on the same page

--- FormEvents/app.js
+++ FormEvents/app.js
@@ -1,25 +1,31 @@
+// const form = document.querySelector("#tweetForm");
+// const input = document.querySelector("#dogName");
+// const list = document.querySelector("#dogs");
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const dogName = input.value;
+//   const newLi = document.createElement("li");
+//   newLi.innerText = dogName;
+//   list.append(newLi);
+//   input.value = "";
+// });
 
-
-const tweetForm = document.querySelector('#tweetForm');
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
+const tweetForm = document.querySelector("#tweetForm");
+const tweetContainer = document.querySelector("#tweets");
+tweetForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = tweetForm.elements.tweet;
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = "";
+  tweetInput.value = "";
 });
 
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
+function addTweet(username, tweet) {
+  const newTweet = document.createElement("li");
+  const bTag = document.createElement("b");
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(` - ${tweet}`);
+  tweetContainer.append(newTweet);
 }
-
