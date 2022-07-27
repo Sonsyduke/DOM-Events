34f02af444926f7c576c723324cd3c45b8b899c4
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-27 13:21:41 +0200

    Change and input events

--- ChangeAndInputEvents/app.js
+++ ChangeAndInputEvents/app.js
@@ -1,10 +1,9 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
+// input.addEventListener("change", function(){
+//     console.log('ksjbvalskv');
 // })
-
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+input.addEventListener("input", function (e) {
+  h1.innerHTML = input.value;
+});
